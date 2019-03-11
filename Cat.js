d168bc77a2c15021e4e67c558028d11c7c0cf8d8
KHANHDAT <khanhdat@example.com>
2019-03-11 18:32:06 +0700

    Let dog eat cat

--- Cat.js
+++ Cat.js
@@ -1,2 +1,3 @@
-function Cat(){
+function Cat() {
 }
+module.exports = Cat;
--- Dog.js
+++ Dog.js
@@ -1,13 +1,13 @@
 var chalk = require('chalk');
-function Dog(name){ 
-    this.name = name; 
+function Dog(name) {
+    this.name = name;
     this.stomach = [];
-    
+
 }
-Dog.prototype.eat = function(cat){
+Dog.prototype.eat = function (cat) {
     this.stomach.push(cat);
 }
-Dog.prototype.sayHi = function(){
-    console.log("Hi! I am a dog."+chalk.blue(this.name));
+Dog.prototype.sayHi = function () {
+    console.log("Hi! I am a dog." + chalk.blue(this.name));
 }
 module.exports = Dog;
